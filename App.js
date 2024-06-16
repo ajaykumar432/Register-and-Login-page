7db622bf6e8b991fe7f916dd0e0e4c206ce1ce1e
ajaykumar432 <127184830+ajaykumar432@users.noreply.github.com>
2024-06-16 22:48:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import App3 from './Component/Routers2/App3'
 // import Login from './Register/Login'
 // import Register from './Register/Register'
 import Main from './Register/Main'
-import Table from './StoreDataInTable/Table'
-import Dropdown from './Dropdown'
-import Carousel1 from './Carousel/Carousel1'
 
 
 
@@ -19,11 +16,6 @@ const App = () => {
      <Register/> */}
      <Main/>
      
-
-     <h1>Table started</h1>
-     <Table/>
-     <Dropdown/>
-     <Carousel1/>
     </>
   )
 }
